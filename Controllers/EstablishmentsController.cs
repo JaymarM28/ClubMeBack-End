b12c897191ac236949a188d035032157200d742e
JaymarM28 <73319065+JaymarM28@users.noreply.github.com>
2025-05-23 22:58:16 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/EstablishmentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Data;
+
+namespace ClubMeBack_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstablishmentsController : Controller
+    {
+        public IConfiguration CurrentConfiguration { get; set; }
+
+        public IDbConnection CurrentConnection { get; set; }
+
+        public EstablishmentsController(IConfiguration configuration, IDbConnection connection)
+        {
+            CurrentConfiguration = configuration;
+            CurrentConnection = connection;
+        }
+
+
+        [HttpPost("CreateEstablishment")]
+        public int CreateEstablishment(string IDEstablecimiento, string NombreEstablecimiento, string DireccionEstablecimiento, string CelularEstablecimiento, string EmailEstablecimiento,
+            string DescripcionEstablecimiento, string OppeningHoursEstablecimiento, bool Activo)
+        {
+            var _context = new Data.ContextEstablishments(CurrentConnection);
+
+            // Assuming you have a method to create an establishment in your data layer
+            int result = _context.sp_CreateEstablishment(IDEstablecimiento, NombreEstablecimiento, DireccionEstablecimiento, CelularEstablecimiento, EmailEstablecimiento, DescripcionEstablecimiento, OppeningHoursEstablecimiento, Activo);
+
+            return result;
+        }
+    }
+}
---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace ClubMeBack_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : Controller
+    {
+        public IConfiguration CurrentConfiguration { get; set; }
+
+        public IDbConnection CurrentConnection { get; set; }
+
+        public ReservationController(IConfiguration configuration, IDbConnection connection)
+        {
+            CurrentConfiguration = configuration;
+            CurrentConnection = connection;
+        }
+
+        [HttpPost("CreateReservation")]
+        public int CreateReservation(string UserId, int TableId, int EstablishmentId, DateTime ReservationDate, DateTime ReservationTime, int PartySize, string SpecialRequests)
+        {
+            int result = 0;
+            try
+            {
+                var _context = new Logica.Reservation(CurrentConnection);
+                // Assuming you have a method to create a reservation in your data layer
+                result = _context.CreateReservation(UserId, TableId, EstablishmentId, ReservationDate, ReservationTime, PartySize, SpecialRequests);
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            return result;
+
+        }
+
+    }
+}
