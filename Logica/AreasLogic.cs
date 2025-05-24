f0849d20a91401e3a8148bfa37ecf9ab40231d60
HaroldParra02 <hamepa0210@gmail.com>
2025-05-24 01:08:28 -0500

    Se crearon los Context y metodos para areas, products

---
+++ Logica/AreasLogic.cs
@@ -0,0 +1,29 @@
+﻿using Clases;
+using Data;
+using StoredProcedures;
+using System.Data;
+
+namespace Logica
+{
+    public class AreasLogic : dbContext
+    {
+        public AreasLogic(IDbConnection connection) : base(connection)
+        {
+        }
+
+        public int CreateAreas(int AreaId, int EstablishmentId, string AreaName, string Description, bool isActive) 
+        {
+            int _ID = 0;
+            try
+            {
+                var _context = new Data.ContextReservation(CurrentConnection);
+                _ID = _context.sp_CreateAreas(AreaId,EstablishmentId,AreaName,Description,isActive);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            return _ID;
+        }
+    }
+}
